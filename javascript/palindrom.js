f9e0d274e44df23490363616b463904f51efbe0f
Jiri Cernik <jiri.cernik@unicorn.com>
2018-06-22 14:54:04 +0200

    Initial commit
    palindrom.js
    - complete palindrome logic
    
    index.html
    - main page

---
+++ javascript/palindrom.js
@@ -0,0 +1,56 @@
+function validatePalindrom(){
+    var text = getInputText();
+
+    if(text && text !== ""){
+        if(text.length === 1){
+            setStatusMessage("Your input is palindrom. But it is only one character long!");
+        } else {
+            if(testPalindrom(text)){
+                setStatusMessage("Yout input is palindrom.");
+            } else {
+                setStatusMessage("Your input is not palindrom.");
+            }
+        }
+    } else {
+        setStatusMessage("Please insert your text into the text field above.");
+    }
+}
+
+function getInputText(){
+    return $("#input-text").val();
+}
+
+/**
+ * This method checks if passed string parameter is palindrom.
+ * @param text : string
+ * @returns {boolean}
+ */
+function testPalindrom(text){
+    text = normalizeText(text);
+    if(text && text.length > 0) {
+        for (var i = 0; i < text.length; i++) {
+            if (text.charAt(i) !== text.charAt(text.length - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+/**
+ * This method normalizes text for palindrom check processing. Method removes diacritics, whitespaces,
+ *
+ * @param text
+ * @returns {string}
+ */
+function normalizeText(text){
+    text = text.normalize("NFD"); //decomposition of characters and diacritics
+    text = text.replace(/([\u0300-\u036f])|(\s)/gm, ""); //removes whitespaces and leftover diacritics
+    text = text.replace(/[^a-zA-Z0-9]/gm, ""); //removes special characters
+    return text.toLowerCase();
+}
+
+function setStatusMessage(message){
+    $("#status-message").text(message);
+}
